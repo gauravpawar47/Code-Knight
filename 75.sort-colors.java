c65088e528adb1b1c5844b270c3bcb12dc6c958f
gauravpawar47 <gauravpawar883@gmail.com>
2024-09-21 11:54:55 +0530

    Add (75, EASY)

---
+++ 75.sort-colors.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=75 lang=java
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+import java.util.Arrays;
+
+class Solution
+{
+    public void sortColors(int[] nums)
+    {
+        Arrays.sort(nums);    
+    }
+}
+// @lc code=end
+
