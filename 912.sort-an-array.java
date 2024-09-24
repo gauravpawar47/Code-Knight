adab29beb71e79da602d102ade17bc4619cf1449
gauravpawar47 <gauravpawar883@gmail.com>
2024-09-24 23:49:59 +0530

    Add (912, MEDIUM)

---
+++ 912.sort-an-array.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=912 lang=java
+ *
+ * [912] Sort an Array
+ */
+
+// @lc code=start
+import java.util.Arrays;
+
+class Solution
+{
+    public int[] sortArray(int[] nums)
+    {
+        Arrays.sort(nums);    
+        return nums;
+    }
+}
+// @lc code=end
+
