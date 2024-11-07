d694532cdaab26c3e84c4a76b8067b0aefd91bb3
gauravpawar47 <gauravpawar883@gmail.com>
2024-11-07 09:34:34 +0530

    DAILY CHALLENGE

---
+++ 238.product-of-array-except-self.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=238 lang=java
+ *
+ * [238] Product of Array Except Self
+ */
+
+// @lc code=start
+class Solution
+{
+    public int[] productExceptSelf(int[] nums)
+    {
+        int[] prefix = new int[nums.length];
+        prefix[0] = 1;
+
+        for(int i = 1; i < nums.length; i++)
+        {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        int rightProduct = 1;
+        for(int i = nums.length - 1; i >= 0; i--)
+        {
+            prefix[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return prefix;
+    }
+}
+// @lc code=end
+
