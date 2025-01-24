3240c53a02288a2ae5de27e0ac56ab015a3f9af0
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-01-24 15:03:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -2,29 +2,39 @@ class Solution
 {
     public int countServers(int[][] grid)
     {
-        int[] Rows = new int[grid.length];
-        int[] Col = new int[grid[0].length];
-        for (int i = 0; i < grid.length; i++) 
+        int m = grid.length;
+        int n = grid[0].length;
+    
+        int server = 0;
+        for(int i = 0; i < m; i++)
         {
-            for (int j = 0; j < grid[0].length; j++) 
-            {
-                Rows[i] += grid[i][j];
-                Col[j] += grid[i][j];
-            }
-        }
-
-        int ans = 0;
-        for (int i = 0; i < grid.length; i++) 
-        {
-            for (int j = 0; j < grid[0].length; j++) 
-            {
-                if (grid[i][j] == 1 && (Rows[i] > 1 || Col[j] > 1)) 
+            for(int j = 0; j < n; j++)
+            {                if(grid[i][j] == 1 && checkIsolated(i, j, grid))
                 {
-                    ans++;
+                    server++;
                 }
             }
         }
-        
-        return ans;
+    }
+
+    public boolean checkIsolated(int row, int coln, int[][] grid)
+    {
+        for(int i = 0; i < grid.length; i++)
+        {
+            if(grid[i][coln] == 1)
+            {
+                return true;
+            }
+        }
+
+        for(int j = 0; j < grid[0].length; j++)
+        {
+            if(grid[row][j] == 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
