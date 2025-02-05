9077e81cbda3d48d6548471ba0c0d5fc7fe86da0
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-02-05 09:54:43 +0530

    Time: 2 ms (13.75%), Space: 41.9 MB (15.64%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -2,24 +2,38 @@ class Solution
 {
     public boolean areAlmostEqual(String s1, String s2)
     {
-        if (s1.equals(s2)) return true;
-        int n = s1.length();
-        for (int i = 0; i < n; i++)
+        if(s1.equals(s2))
         {
-            for (int j = i + 1; j < n; j++)
-            {  
-                char[] arr = s2.toCharArray();
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                
-                if (s1.equals(new String(arr)))
+            return true;
+        }
+        
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        ArrayList<Integer> misMatch = new ArrayList<>();
+ 
+        for(int i = 0; i < s1.length(); i++)
+        {
+            char c1 = s1.charAt(i);
+            char c2 = s2.charAt(i);
+
+            map1.put(c1, map1.getOrDefault(c1, 0) + 1);
+            map2.put(c2, map2.getOrDefault(c2, 0) + 1);
+        
+            if(c1 != c2)
+            {
+                misMatch.add(i);
+                if(misMatch.size() > 2)
                 {
-                    return true;
+                    return false;
                 }
             }
         }
-        
-        return false; 
+
+        if(!map1.equals(map2))
+        {
+            return false;
+        }
+
+        return misMatch.size() == 2 && s1.charAt(misMatch.get(0)) == s2.charAt(misMatch.get(1)) && s1.charAt(misMatch.get(1)) == s2.charAt(misMatch.get(0)); 
     }
 }
