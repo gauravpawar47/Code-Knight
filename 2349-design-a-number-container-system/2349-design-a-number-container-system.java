8d5f83e39b5dc3c35908662f5366c68e54b83be2
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-02-08 11:46:29 +0530

    Time: 80 ms (63.01%), Space: 98.6 MB (66.44%) - LeetHub

---
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.java
@@ -0,0 +1,38 @@
+class NumberContainers
+{
+    private Map<Integer, Integer> idxToNum;
+    private Map<Integer, TreeSet<Integer>> numToIdx;
+
+    public NumberContainers()
+    {
+        idxToNum = new HashMap<>();
+        numToIdx = new HashMap<>();
+    }
+    
+    public void change(int index, int number)
+    {
+        if(idxToNum.containsKey(index))
+        {
+            int prevNum = idxToNum.get(index);
+            numToIdx.get(prevNum).remove(index); // O(log(n))
+            if(numToIdx.get(prevNum).isEmpty())
+            {
+                numToIdx.remove(prevNum); // O(1)
+            }
+        }
+
+        idxToNum.put(index, number);
+        numToIdx.putIfAbsent(number, new TreeSet<>());
+        numToIdx.get(number).add(index); // O(log(n))
+    }
+    
+    public int find(int number)
+    {
+        if(numToIdx.containsKey(number))
+        {
+            return numToIdx.get(number).first(); // O(1)
+        }
+        
+        return -1;
+    }
+}
