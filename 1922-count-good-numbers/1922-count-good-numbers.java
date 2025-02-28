d9acf302063454112a418d10e961ef0f9e87dd5d
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-02-28 17:43:04 +0530

    Time: 0 ms (100%), Space: 40.3 MB (91.67%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,36 @@
+ class Solution
+{
+    public long MOD = 1_000_000_007;
+
+    public long pow(long x, long n)
+    {
+        // Step 1 : Base-Case
+        if(n == 0)
+        {
+            return 1;
+        }
+
+        // Step 2 : Kaam & Inner Function Call
+        long halfPower = pow(x, n / 2);
+        if(n % 2 == 0)
+        {
+            return (halfPower * halfPower) % MOD;
+        }
+        else
+        {
+            return (halfPower * halfPower * x) % MOD;
+        }
+
+    }
+
+    public int countGoodNumbers(long n)
+    {
+        long even = (n + 1) / 2;
+        long odd = (n) / 2;
+
+        long evenHalf = pow(5, even) % MOD;
+        long oddHalf = pow(4, odd) % MOD;
+
+        return (int) ((evenHalf * oddHalf) % MOD);
+    }
+}
