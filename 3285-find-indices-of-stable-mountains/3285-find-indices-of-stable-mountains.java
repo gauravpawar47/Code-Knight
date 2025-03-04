6088ddd147521beacc9b96a9ae78ffeab19f11ca
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-03-04 15:34:09 +0530

    Time: 1 ms (100%), Space: 45.2 MB (7.86%) - LeetHub

---
+++ 3285-find-indices-of-stable-mountains/3285-find-indices-of-stable-mountains.java
@@ -0,0 +1,16 @@
+class Solution 
+{
+    public List<Integer> stableMountains(int[] height, int threshold) 
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        for(int i = 1; i < height.length; i++)
+        {
+            if(height[i - 1] > threshold)
+            {
+                result.add(i);
+            }
+        }
+
+        return result;   
+    }
+}
