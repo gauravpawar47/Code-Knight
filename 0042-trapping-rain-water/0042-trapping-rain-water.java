0ab076b871d43239785e3de495864c3fd5ccfc31
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-04-02 19:04:24 +0530

    Time: 6 ms (7.12%), Space: 45.9 MB (88.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,31 +2,25 @@ class Solution
 {
     public int trap(int[] height)
     {
-        // Step 1 : Calculate Left-Max Boundary
-        int n = height.length;
-        int[] leftMax = new int[n];
-
-        leftMax[0] = height[0];
-        for(int i = 1; i < n; i++)
-        {
-            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
-        }
-
-        // Step 2 : Calculate Right-Max Boundary
-        int[] rightMax = new int[n];
-
-        rightMax[n - 1] = height[n - 1];
-        for(int i = n - 2; i >= 0; i--)
-        {
-            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
-        }
-
-        // Step 3 : Calculate Trapped Water
+        Stack<Integer> stack = new Stack<>();
         int trappedWater = 0;
-        for(int i = 0; i < n; i++)
+
+        for(int i = 0; i < height.length; i++)
         {
-            int currentWater = Math.min(leftMax[i], rightMax[i]) - height[i]; 
-            trappedWater += currentWater;
+            while(!stack.isEmpty() && height[i] > height[stack.peek()])
+            {
+                int top = stack.pop();
+                if(stack.isEmpty())
+                {
+                    break;
+                }
+
+                int distance = i - stack.peek() - 1;
+                int boundHeight = Math.min(height[i], height[stack.peek()]) - height[top];
+                trappedWater += distance * boundHeight;
+            }
+
+            stack.push(i);
         }
 
         return trappedWater;
