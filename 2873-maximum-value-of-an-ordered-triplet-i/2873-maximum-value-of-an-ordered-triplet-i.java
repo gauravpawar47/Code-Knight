901cd334ea002dfb2d154c48585e852e4a8097df
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-04-03 08:48:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.java
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.java
@@ -3,22 +3,20 @@ class Solution
     public long maximumTripletValue(int[] nums) 
     {
         int n = nums.length;
-        if (n < 3)
+        if(n < 3)
         {
-            return 0; // Not enough elements for a triplet
+            return 0;
         }
-
-        long maxTriplet = 0;
-
-        for (int i = 0; i < n - 2; i++) 
+    
+        long maxTriplet = Long.MIN_VALUE;
+        for(int i = 0; i < n - 2; i++)
         {
-            for (int j = i + 1; j < n - 1; j++) 
+            for(int j = i + 1; j < n - 2; j++)
             {
-                long diff = (long) nums[i] - nums[j]; // A[i] - A[j]
-
-                // Find max(A[k]) where k > j
+                long diff = (long) nums[i] - nums[j];
                 long maxK = Long.MIN_VALUE;
-                for (int k = j + 1; k < n; k++) 
+
+                for(int k = j + 1; k < n; k++)
                 {
                     maxK = Math.max(maxK, nums[k]);
                 }
@@ -27,6 +25,6 @@ class Solution
             }
         }
 
-        return Math.max(0, maxTriplet); // Ensure non-negative result
+        return Math.max(0, maxTriplet);
     }
 }
