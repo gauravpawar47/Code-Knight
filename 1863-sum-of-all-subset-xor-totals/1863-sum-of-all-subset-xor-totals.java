0081875abe4002e7dd582c4d5aed1c3e87309f71
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-04-05 10:07:01 +0530

    Time: 14 ms (12.03%), Space: 45 MB (9.61%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,23 +1,40 @@
 class Solution 
 {
-    public int subsetXORSum(int[] nums) 
-    {
-        return backtrack(nums, 0, 0);    
-    }
-
-    public static int backtrack(int[] nums, int i, int sum)
+    public void populate(int[] nums, int i, ArrayList<ArrayList<Integer>> subsets, ArrayList<Integer> currSubset)
     {
         // Step 1 : Base-Case
         if(i == nums.length)
         {
-            return sum;
+            subsets.add(new ArrayList<>(currSubset));
+            return; 
         }
 
-        // Step 2 : Kaam
-        int include = backtrack(nums, i + 1, sum ^ nums[i]);
-        int exclude = backtrack(nums, i + 1, sum);
+        // Step 2 : Kaam & Inner Function Call
+        currSubset.add(nums[i]);
+        populate(nums, i + 1, subsets, currSubset);
 
-        // Step 3 : Inner Function Call
-        return include + exclude;
+        // Step 3 : Backtrack Step
+        currSubset.remove(currSubset.size() - 1);
+        populate(nums, i + 1, subsets, currSubset);   
+    }
+
+    public int subsetXORSum(int[] nums) 
+    {
+        ArrayList<ArrayList<Integer>> subsets = new ArrayList<>();
+        ArrayList<Integer> currSubset = new ArrayList<>();
+        populate(nums, 0, subsets, currSubset);
+
+        int result = 0;
+        for(ArrayList<Integer> subset : subsets)
+        {
+            int xor = 0 ;
+            for(int n : subset)
+            {
+                xor ^= n;
+            }
+
+            result += xor;
+        }
+        return result;
     }
 }
