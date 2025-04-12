3bc6b5a300cda47fb645c411544c5dfa3f719204
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-04-12 08:26:10 +0530

    Time: 0 ms (100%), Space: 43.6 MB (18.16%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -12,39 +12,27 @@ class Solution
 {
     public ListNode deleteDuplicates(ListNode head) 
     {
-        ListNode T = head;
-        ArrayList<Integer> list = new ArrayList<>();
-
-        while(T != null)
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        
+        dummy.next = head;
+        while(head != null)
         {
-            list.add(T.val);
-            T = T.next;
-        }
-
-        ArrayList<Integer> unique = new ArrayList<>();
-        int i = 0;
-        while(i < list.size())
-        {
-            int count = 1;
-            while(i + count < list.size() && list.get(i).equals(list.get(i + count)))
+            if(head.next != null && head.val == head.next.val)
             {
-                count++;
-            }
+                int duplicate = head.val;
+                while(head != null && duplicate == head.val)
+                {
+                    head = head.next;
+                }
 
-            if(count == 1)
+                prev.next = head;
+            }
+            else
             {
-                unique.add(list.get(i));
+                prev = prev.next;
+                head = head.next;
             }
-
-            i += count;
-        }
-
-        ListNode temp = new ListNode(0);
-        ListNode dummy = temp;
-        for(i = 0; i < unique.size(); i++)
-        {
-            temp.next = new ListNode(unique.get(i));
-            temp = temp.next;
         }
 
         return dummy.next;
