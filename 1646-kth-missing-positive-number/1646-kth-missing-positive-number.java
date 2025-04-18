bce897c62a327cb898ccd12bec244a97f514d933
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-04-18 15:40:04 +0530

    Time: 0 ms (100%), Space: 42.7 MB (86.39%) - LeetHub

---
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public int findKthPositive(int[] arr, int k) 
+    {
+        int expected = 1;   // Start checking from 1 (smallest positive number)
+        int index = 0;      // Pointer for arr[]
+        
+        while (k > 0) 
+        {
+            // If expected number is present in array, skip it
+            if (index < arr.length && arr[index] == expected) 
+            {
+                index++;
+            } 
+            else 
+            {
+                // Otherwise, it's missing
+                k--;
+            }
+
+            // If we haven't found the kth missing, move to next number
+            expected++;
+        }
+        
+        // When k reaches 0, expected is 1 more than the answer
+        return expected - 1;
+    }
+}
