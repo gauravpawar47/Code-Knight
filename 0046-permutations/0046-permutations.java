1243524b820ca12947313dd8e7d9c1a8c8f5e847
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-04-19 08:52:34 +0530

    Time: 1 ms (93.15%), Space: 44.7 MB (31.57%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,37 +1,40 @@
 class Solution 
 {
-    public static void permutations(int[] nums, List<List<Integer>> result, List<Integer> current)
+    public void backtrack(int[] nums, boolean[] used, List<Integer> curr, List<List<Integer>> result)
     {
         // Step 1 : Base-Case
-        if(nums.length == 0)
+        if(curr.size() == nums.length)
         {
-            result.add(new ArrayList<>(current));
+            result.add(new ArrayList<>(curr));
             return;
         }
 
         // Step 2 : Kaam
         for(int i = 0; i < nums.length; i++)
         {
-            int curr = nums[i];
-            int[] arr = new int[nums.length - 1];
+            // Step 3 : Check if Number is Already present or not
+            if(used[i])
+            {
+                continue;
+            }
 
-            System.arraycopy(nums, 0, arr, 0, i);
-            System.arraycopy(nums, i + 1, arr, i, nums.length - i - 1);
+            // Step 4 : Include Call
+            used[i] = true;
+            curr.add(nums[i]);
 
-            current.add(curr);
+            // Step 5 : Explore
+            backtrack(nums, used, curr, result);
 
-            // Step 3 : Inner Function Call
-            permutations(arr, result, current);
-
-            // Step 4 : Backtrack Step
-            current.remove(current.size() - 1);
+            // Step 6 : Backtrack Step
+            used[i] = false;
+            curr.remove(curr.size() - 1);
         }
     }
 
     public List<List<Integer>> permute(int[] nums) 
     {
         List<List<Integer>> result = new ArrayList<>();
-        permutations(nums, result, new ArrayList<>());
-        return result;
+        backtrack(nums, new boolean[nums.length], new ArrayList<>(), result);
+        return result;    
     }
 }
