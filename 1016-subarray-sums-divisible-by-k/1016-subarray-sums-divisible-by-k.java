620f8e3bbef74a7f2524713cfac6c7c2c161cf48
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-05-15 18:54:44 +0530

    Time: 24 ms (53.09%), Space: 50.1 MB (7.42%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -1,40 +1,27 @@
-public class Solution
+class Solution 
 {
-    public int subarraysDivByK(int[] nums, int k)
+    public int subarraysDivByK(int[] nums, int k) 
     {
-        // Initialize count of subarrays, prefix sum, and hash map for remainders
-        int count = 0;
-        int prefixSum = 0;
-    
-        HashMap<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, 1); // To handle subarrays that start from the beginning
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        int sum = 0;
+        int ans = 0;
 
-        for (int num : nums)
+        for(int i = 0; i < nums.length; i++)
         {
-            // Update prefix sum
-            prefixSum += num;
-            
-            // Calculate the remainder of the prefix sum divided by k
-            int mod = prefixSum % k;
-            
-            // Adjust negative remainders to be positive
-            if (mod < 0)
+            sum += nums[i];
+            int mod = sum % k;
+
+            if(mod < 0)
             {
                 mod += k;
             }
             
-            // If this remainder has been seen before, it means there are subarrays ending here that are divisible by k
-            if (prefixMap.containsKey(mod))
-            {
-                count += prefixMap.get(mod);
-                prefixMap.put(mod, prefixMap.get(mod) + 1);
-            }
-            else
-            {
-                prefixMap.put(mod, 1);
-            }
+            ans += map.getOrDefault(mod, 0);
+            map.put(mod, map.getOrDefault(mod, 0) + 1);
         }
-        
-        return count; // Total number of subarrays divisible by k
+
+        return ans;
     }
 }
