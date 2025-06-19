0d1ed11810b976e4d4c9905c3de3c1f51cb492a1
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-06-19 18:45:32 +0530

    Time: 1 ms (47.91%), Space: 40.7 MB (35.93%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,17 +1,33 @@
-class Solution {
-    public int integerReplacement(int n) {
+class Solution 
+{
+    public int integerReplacement(int n) 
+    {
         return helper(n, new HashMap<>());
     }
 
-    private int helper(long n, Map<Long, Integer> memo) {
-        if (n == 1) return 0;
-        if (memo.containsKey(n)) return memo.get(n);
+    private int helper(long n, Map<Long, Integer> memo) 
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
+
+        if (memo.containsKey(n))
+        {
+            return memo.get(n);
+        } 
 
         int ans;
-        if (n % 2 == 0) {
+        if (n % 2 == 0) 
+        {
             ans = 1 + helper(n / 2, memo);
-        } else {
-            ans = 1 + Math.min(helper(n + 1, memo), helper(n - 1, memo));
+        } 
+        else 
+        {
+            int op1 = helper(n + 1, memo);
+            int op2 = helper(n - 1, memo);
+
+            ans = 1 + Math.min(op1, op2);
         }
 
         memo.put(n, ans);
