20a890c35638f1215df544ec3612c692bb1fffa4
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-07-12 19:50:34 +0530

    Time: 3 ms (3.05%), Space: 57.1 MB (43.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,36 +1,49 @@
-class Solution
+class Solution 
 {
-    public int maxSubArray(int[] nums)
+    public boolean isAllNegative(int[] nums)
     {
-        int currSum = 0;
+        for(int n : nums)
+        {
+            if(n > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int smallestNegative(int[] nums)
+    {
+        int min = Integer.MAX_VALUE;
+        for(int n : nums)
+        {
+            min = Math.min(min, Math.abs(n));
+        }
+
+        return min;
+    }
+    
+    public int maxSubArray(int[] nums) 
+    {
+        if(isAllNegative(nums))
+        {
+            return -smallestNegative(nums);
+        }
+
+        int sum = 0;
         int maxSum = Integer.MIN_VALUE;
         for(int i = 0; i < nums.length; i++)
         {
-            currSum += nums[i];
-            if(currSum < 0)
+            sum += nums[i];
+            if(sum < 0)
             {
-                currSum = 0;
+                sum = 0;
             }
 
-            maxSum = Math.max(maxSum, currSum);
-        }    
-
-        return maxSum == 0 ? maxNegative(nums) : maxSum;
-    }
-
-    public int maxNegative(int[] arr)
-    {
-        if(arr.length == 1)
-        {
-            return arr[0];
+            maxSum = Math.max(maxSum, sum);
         }
 
-        int maxNeg = Integer.MIN_VALUE;
-        for(int i = 0; i < arr.length; i++)
-        {
-            maxNeg = Math.max(maxNeg, arr[i]);
-        }
-
-        return maxNeg;
+        return maxSum;
     }
 }
