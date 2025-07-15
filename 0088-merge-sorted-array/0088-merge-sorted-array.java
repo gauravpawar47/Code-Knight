4a927be54293f11d34ecdb2151093cccf7a26cad
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-07-15 11:33:15 +0530

    Time: 0 ms (100%), Space: 42.4 MB (26.98%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -18,12 +18,68 @@ class Solution
             arr[j] = nums2[j - m];
         }
 
-        Arrays.sort(arr);
-        for(int i = 0; i < m + n; i++)
+        mergeSort(arr, 0, (m + n) - 1);
+        for(int i = 0; i < m; i++)
         {
             nums1[i] = arr[i];
         }
+
+        for(int j = m; j < m + n; j++)
+        {
+            nums1[j] = arr[j];
+        }
+    }   
+
+    public void mergeSort(int[] arr, int si, int ei)
+    {
+        if(si >= ei)
+        {
+            return;
+        }
+
+        int mid = (si + ei) / 2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
     }
 
-    
+    public void merge(int[] arr, int si, int mid, int ei)
+    {
+        int i = si;
+        int j = mid + 1;
+
+        int[] temp = new int[ei - si + 1];
+        int k = 0;
+
+        while(i <= mid && j <= ei)
+        {
+            if(arr[i] < arr[j])
+            {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(i <= mid)
+        {
+            temp[k++] = arr[i++];
+        }
+
+        while(j <= ei)
+        {
+            temp[k++] = arr[j++];
+        }
+
+        for(i = si; i <= ei; i++)
+        {
+            arr[i] = temp[i - si];
+        }
+    }
 }
