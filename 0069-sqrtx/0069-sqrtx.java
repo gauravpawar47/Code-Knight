de36b2c961e1df0a72520994572d7d45af641b1a
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-07-19 15:30:19 +0530

    Time: 57 ms (5.11%), Space: 41 MB (40.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -2,6 +2,24 @@ class Solution
 {
     public int mySqrt(int x) 
     {
-        return (int) Math.sqrt(x);
+        if(x == 1 || x == 2 || x == 3)
+        {
+            return 1;
+        }
+
+        for(long i = 0; i <= x / 2 + 1; i++)
+        {
+            if(i * i > x)
+            {
+                return (int) i - 1;
+            }
+
+            if(i * i == x)
+            {
+                return (int) i;
+            }
+        }
+
+        return -1;
     }
 }
