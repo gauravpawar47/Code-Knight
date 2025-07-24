fff8062a36e7cc6eb998fbfcdb7f2f2997561c62
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-07-24 15:11:30 +0530

    Time: 42 ms (72.41%), Space: 44.7 MB (79.31%) - LeetHub

---
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Solution {
+    int[] subtreeXor;
+    int[] tin, tout, parent;
+    int timer;
+    List<Integer>[] graph;
+
+    public int minimumScore(int[] nums, int[][] edges) {
+        int n = nums.length;
+        subtreeXor = new int[n];
+        tin = new int[n];
+        tout = new int[n];
+        parent = new int[n];
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; ++i) graph[i] = new ArrayList<>();
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        timer = 0;
+        dfs(0, -1, nums);
+
+        int totalXor = subtreeXor[0];
+
+        // Identify child nodes in edges for consistent orientation
+        int[][] edgeEnds = new int[edges.length][2];
+        for (int i = 0; i < edges.length; ++i) {
+            int u = edges[i][0], v = edges[i][1];
+            if (parent[v] == u) edgeEnds[i] = new int[]{u, v};
+            else edgeEnds[i] = new int[]{v, u};
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < edges.length; ++i) {
+            int a = edgeEnds[i][1];
+            for (int j = i + 1; j < edges.length; ++j) {
+                int b = edgeEnds[j][1];
+                int comp1, comp2, comp3;
+                if (isAncestor(a, b)) {
+                    comp1 = subtreeXor[b];
+                    comp2 = subtreeXor[a] ^ subtreeXor[b];
+                    comp3 = totalXor ^ subtreeXor[a];
+                } else if (isAncestor(b, a)) {
+                    comp1 = subtreeXor[a];
+                    comp2 = subtreeXor[b] ^ subtreeXor[a];
+                    comp3 = totalXor ^ subtreeXor[b];
+                } else {
+                    comp1 = subtreeXor[a];
+                    comp2 = subtreeXor[b];
+                    comp3 = totalXor ^ subtreeXor[a] ^ subtreeXor[b];
+                }
+                int maxVal = Math.max(comp1, Math.max(comp2, comp3));
+                int minVal = Math.min(comp1, Math.min(comp2, comp3));
+                ans = Math.min(ans, maxVal - minVal);
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(int node, int par, int[] nums) {
+        tin[node] = ++timer;
+        subtreeXor[node] = nums[node];
+        parent[node] = par;
+        for (int nei : graph[node]) {
+            if (nei == par) continue;
+            dfs(nei, node, nums);
+            subtreeXor[node] ^= subtreeXor[nei];
+        }
+        tout[node] = ++timer;
+    }
+
+    private boolean isAncestor(int u, int v) {
+        return tin[u] <= tin[v] && tout[v] <= tout[u];
+    }
+}
