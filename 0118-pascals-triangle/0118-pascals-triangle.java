34a6d4f4b21c44c377f96fcaf11e63a7a54b7c76
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-08-01 23:26:45 +0530

    Time: 1 ms (82.66%), Space: 42.4 MB (7.56%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -2,27 +2,33 @@ class Solution
 {
     public List<List<Integer>> generate(int numRows) 
     {
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i = 1; i <= numRows; i++)
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int rowNum = 1; rowNum < numRows; rowNum++) 
         {
-            ArrayList<Integer> curr = new ArrayList<>();
-            for(int j = 1; j <= i; j++)
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum - 1);
+
+            // The first row element is always 1.
+            row.add(1);
+
+            // Each triangle element (other than the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and
+            // above-and-to-the-right.
+            for (int j = 1; j < rowNum; j++) 
             {
-                if(j == 1 || j == i)
-                {
-                    curr.add(1);
-                }
-                else
-                {
-                    int left = result.get(i - 2).get(j - 2);
-                    int right = result.get(i - 2).get(j - 1);
-                    curr.add(left + right);
-                }
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
             }
 
-            result.add(curr);
-        }    
+            // The last row element is always 1.
+            row.add(1);
 
-        return result;
+            triangle.add(row);
+        }
+
+        return triangle;
     }
 }
