10d97c848bee11d5b037c574685cbec360f68739
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-09-14 10:50:59 +0530

    Time: 27 ms (47.87%), Space: 48 MB (70.62%) - LeetHub

---
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.java
@@ -0,0 +1,70 @@
+class Solution 
+{
+    public String[] spellchecker(String[] wordlist, String[] queries) 
+    {
+        HashSet<String> exact = new HashSet<>(Arrays.asList(wordlist));
+        HashMap<String, String> caseMap = new HashMap<>();
+        HashMap<String, String> vowelMap = new HashMap<>();
+
+        for (String w : wordlist) 
+        {
+            String lower = toLower(w);
+            String devowel = deVowel(lower);
+            caseMap.putIfAbsent(lower, w);
+            vowelMap.putIfAbsent(devowel, w);
+        }
+        
+        String[] result = new String[queries.length];
+        for (int i = 0; i < queries.length; i++) 
+        {
+            String q = queries[i];
+            if (exact.contains(q)) 
+            {
+                result[i] = q;
+            } 
+            else 
+            {
+                String lower = toLower(q);
+                String devowel = deVowel(lower);
+                if (caseMap.containsKey(lower))
+                {
+                    result[i] = caseMap.get(lower);
+                } 
+                else if (vowelMap.containsKey(devowel))
+                {
+                    result[i] = vowelMap.get(devowel);
+                } 
+                else
+                {
+                    result[i] = "";
+                } 
+            }
+        }
+
+        return result;
+    }
+
+    public String toLower(String s) 
+    {
+        return s.toLowerCase();
+    }
+    
+    public String deVowel(String s) 
+    {
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i++) 
+        {
+            if (isVowel(ch[i]))
+            {
+                ch[i] = '*';
+            } 
+        }
+
+        return new String(ch);
+    }
+
+    public boolean isVowel(char c) 
+    {
+        return "aeiou".indexOf(Character.toLowerCase(c)) >= 0;
+    }
+}
