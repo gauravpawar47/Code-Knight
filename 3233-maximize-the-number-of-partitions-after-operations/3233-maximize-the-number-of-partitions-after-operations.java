245f2cd803ebc2ad96eb47d4fc576811f95d82b4
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-10-17 23:59:52 +0530

    Time: 5 ms (53.85%), Space: 45 MB (53.85%) - LeetHub

--- 3233-maximize-the-number-of-partitions-after-operations/3233-maximize-the-number-of-partitions-after-operations.java
+++ 3233-maximize-the-number-of-partitions-after-operations/3233-maximize-the-number-of-partitions-after-operations.java
@@ -9,18 +9,24 @@ public class Solution
         int num = 0;
         int mask = 0;
         int count = 0;
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n - 1; i++) 
+        {
             int binary = 1 << (s.charAt(i) - 'a');
-            if ((mask & binary) == 0) {
+            if ((mask & binary) == 0) 
+            {
                 count++;
-                if (count <= k) {
+                if (count <= k) 
+                {
                     mask |= binary;
-                } else {
+                } 
+                else 
+                {
                     num++;
                     mask = binary;
                     count = 1;
                 }
             }
+
             left[i + 1][0] = num;
             left[i + 1][1] = mask;
             left[i + 1][2] = count;
@@ -29,13 +35,18 @@ public class Solution
         num = 0;
         mask = 0;
         count = 0;
-        for (int i = n - 1; i > 0; i--) {
+        for (int i = n - 1; i > 0; i--) 
+        {
             int binary = 1 << (s.charAt(i) - 'a');
-            if ((mask & binary) == 0) {
+            if ((mask & binary) == 0) 
+            {
                 count++;
-                if (count <= k) {
+                if (count <= k) 
+                {
                     mask |= binary;
-                } else {
+                } 
+                else 
+                {
                     num++;
                     mask = binary;
                     count = 1;
@@ -47,17 +58,24 @@ public class Solution
         }
 
         int maxVal = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             int seg = left[i][0] + right[i][0] + 2;
             int totMask = left[i][1] | right[i][1];
             int totCount = Integer.bitCount(totMask);
-            if (left[i][2] == k && right[i][2] == k && totCount < 26) {
+            
+            if (left[i][2] == k && right[i][2] == k && totCount < 26) 
+            {
                 seg++;
-            } else if (Math.min(totCount + 1, 26) <= k) {
+            } 
+            else if (Math.min(totCount + 1, 26) <= k) 
+            {
                 seg--;
             }
+            
             maxVal = Math.max(maxVal, seg);
         }
+        
         return maxVal;
     }
 }
