abb7d2f31e3895f4a0fe0628dec9061af7b07c98
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-10-28 08:46:47 +0530

    Time: 0 ms (100%), Space: 42.1 MB (80.12%) - LeetHub

---
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
@@ -0,0 +1,34 @@
+class Solution 
+{
+    public int countValidSelections(int[] nums) 
+    {
+        int total = 0;
+        int ls = 0;
+        int cases = 0;
+        
+        for (int num : nums)
+        {
+            total += num;
+        } 
+
+        for (int num : nums) 
+        {
+            int rs = total - ls - num;
+            if (num == 0) 
+            {
+                if (ls == rs) 
+                {
+                    cases += 2;
+                } 
+                else if (ls == rs - 1 || ls - 1 == rs) 
+                {
+                    cases += 1;
+                }
+            }
+
+            ls += num;
+        }
+        
+        return cases;
+    }
+}
