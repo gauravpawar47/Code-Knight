c4c4a3c90e8ba7890040152d0b4f57da65229d73
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2025-11-10 22:30:14 +0530

    Time: 0 ms (100%), Space: 42.4 MB (11.97%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
@@ -1,8 +1,10 @@
-class Solution {
-
-    public int countOperations(int num1, int num2) {
+class Solution 
+{
+    public int countOperations(int num1, int num2) 
+    {
         int res = 0; // total number of subtraction operations
-        while (num1 != 0 && num2 != 0) {
+        while (num1 != 0 && num2 != 0) 
+        {
             // each step of the Euclidean algorithm
             res += num1 / num2;
             num1 %= num2;
@@ -11,6 +13,7 @@ class Solution {
             num1 = num2;
             num2 = temp;
         }
+        
         return res;
     }
 }
