c76f5c0af750c979e678e2b22a48c9ad85f22d57
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2026-02-15 10:56:35 +0530

    Time: 4 ms (14.34%), Space: 45.5 MB (5.6%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,12 +1,39 @@
-import java.math.BigInteger;
-
-class Solution
+class Solution 
 {
-    public String addBinary(String a, String b)
+    public String addBinary(String a, String b) 
     {
-        BigInteger n1 = new BigInteger(a, 2);    
-        BigInteger n2 = new BigInteger(b, 2);        
+        int n1 = a.length();
+        int n2 = b.length();
+        
+        int max = Math.max(n1, n2);
+        int C_in = 0;
+        int i = 0;
+        String res = "";
 
-        return n1.add(n2).toString(2);
+        while (i < max || C_in > 0) 
+        {
+            int A = 0;
+            int B = 0;
+
+            if (i < n1)
+            {
+                A = a.charAt(n1 - 1 - i) - '0';
+            }
+
+            if (i < n2)
+            {
+                B = b.charAt(n2 - 1 - i) - '0';
+            }
+
+            int S = (A ^ B) ^ C_in;
+            int C_out = ((A ^ B) & C_in) | (A & B);
+
+            res = (char)(S + '0') + res;
+            C_in = C_out;
+
+            i++;
+        }
+
+        return res;
     }
 }
