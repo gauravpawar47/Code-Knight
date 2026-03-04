a20ea615161b8a62128e5247c76154902ff909c2
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2026-03-04 22:26:57 +0530

    Time: 4 ms (15.16%), Space: 43.6 MB (95.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,12 +1,7 @@
 class Solution 
 {
     public void merge(int[] nums1, int m, int[] nums2, int n) 
-    {
-        if(n == 0)
-        {
-            return;
-        }
-
+    {  
         int[] arr = new int[m + n];
         for(int i = 0; i < m; i++)
         {
@@ -18,68 +13,11 @@ class Solution
             arr[j] = nums2[j - m];
         }
 
-        mergeSort(arr, 0, (m + n) - 1);
-        for(int i = 0; i < m; i++)
+        Arrays.sort(arr);
+        for(int i = 0; i < m + n; i++)
         {
             nums1[i] = arr[i];
         }
-
-        for(int j = m; j < m + n; j++)
-        {
-            nums1[j] = arr[j];
-        }
-    }   
-
-    public void mergeSort(int[] arr, int si, int ei)
-    {
-        if(si >= ei)
-        {
-            return;
-        }
-
-        int mid = (si + ei) / 2;
-        mergeSort(arr, si, mid);
-        mergeSort(arr, mid + 1, ei);
-        merge(arr, si, mid, ei);
-    }
-
-    public void merge(int[] arr, int si, int mid, int ei)
-    {
-        int i = si;
-        int j = mid + 1;
-
-        int[] temp = new int[ei - si + 1];
-        int k = 0;
-
-        while(i <= mid && j <= ei)
-        {
-            if(arr[i] < arr[j])
-            {
-                temp[k] = arr[i];
-                i++;
-                k++;
-            }
-            else
-            {
-                temp[k] = arr[j];
-                j++;
-                k++;
-            }
-        }
-
-        while(i <= mid)
-        {
-            temp[k++] = arr[i++];
-        }
-
-        while(j <= ei)
-        {
-            temp[k++] = arr[j++];
-        }
-
-        for(i = si; i <= ei; i++)
-        {
-            arr[i] = temp[i - si];
-        }
+        
     }
 }
