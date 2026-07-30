b77d77fd08d2d7c66ff4ab4973b8d5fcc9232efe
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2026-07-30 22:31:39 +0530

    Time: 0 ms (100%), Space: 43.1 MB (66.51%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,9 @@
+class Solution 
+{
+    public int minimumPushes(String A) 
+    {
+        int q = A.length() >> 3;
+        int r = A.length() & 7;
+        return ((q << 2) + r) * (q + 1);
+    }
+}
