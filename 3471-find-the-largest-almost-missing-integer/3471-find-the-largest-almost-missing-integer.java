24b015b1572bd2aa7c4f4ffc0cd3c55d703fd392
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2026-08-18 08:13:05 +0530

    Time: 2 ms (72.63%), Space: 45.2 MB (48.42%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,30 +1,56 @@
-class Solution
+class Solution 
 {
     public int largestInteger(int[] nums, int k) 
     {
-        int[] map = new int[51];
-        for(int i = 0; i <= nums.length - k; i++)
-        {
-            int j = i;
-            HashSet<Integer> seen = new HashSet<>();
-            while(j < i + k)
-            {
-                if(!seen.contains(nums[j]))
-                {
-                    map[nums[j]]++;
-                    seen.add(nums[j]);
-                }
-                
-                j++;
-            }
-        }    
+        int n = nums.length;
+        HashMap<Integer, Integer> mp = new HashMap<>();
 
-        for(int i = map.length - 1; i >= 0; i--)
+        for (int i = 0; i < n; i++) 
         {
-            if(map[i] == 1)
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        }
+
+        if (k == nums.length) 
+        {
+            int maxValue = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++) 
             {
-                return i;
+                maxValue = Math.max(maxValue, nums[i]);
             }
+
+            return maxValue;
+        }
+
+        if (k == 1) 
+        {
+            int maxValue = -1;
+            for (int i = 0; i < n; i++) 
+            {
+                if (mp.get(nums[i]) == 1 && nums[i] > maxValue) 
+                {
+                    maxValue = nums[i];
+                }
+            }
+
+            return maxValue;
+        }
+
+        n = n - 1;
+        if (nums[0] == nums[n]) 
+        {
+            return -1;
+        }
+        if (mp.get(nums[0]) == 1 && mp.get(nums[n]) == 1) 
+        {
+            return Math.max(nums[0], nums[n]);
+        }
+        if (mp.get(nums[0]) == 1 && mp.get(nums[n]) > 1) 
+        {
+            return nums[0];
+        }
+        if (mp.get(nums[n]) == 1 && mp.get(nums[0]) > 1) 
+        {
+            return nums[n];
         }
 
         return -1;
