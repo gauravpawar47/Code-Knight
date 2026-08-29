4a9fb0c6fb4c7d9e348d7e15dc81f7ecd4ecf67d
Gaurav Pawar <84630193+gauravpawar47@users.noreply.github.com>
2026-08-29 22:13:47 +0530

    Time: 74 ms (84.85%), Space: 210.1 MB (25.76%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,54 +1,34 @@
-class Solution
+class Solution 
 {
-    public int[] lexicographicallySmallestArray(int[] nums, int limit)
+    public int[] lexicographicallySmallestArray(int[] A, int limit) 
     {
-        int n = nums.length;
-        int[][] valueIndexPairs = new int[n][2];
-        for(int i = 0; i < n; i++)
-        {
-            valueIndexPairs[i][0] = nums[i];
-            valueIndexPairs[i][1] = i;
-        }   
+        int[] srt = A.clone();
+        Arrays.sort(srt);
+        ArrayList<List<Integer>> grps = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int id = -1;
 
-        Arrays.sort(valueIndexPairs, (a, b) -> a[0] - b[0]);
-        ArrayList<ArrayList<int[]>> groups = new ArrayList<>();
-        ArrayList<int[]> current = new ArrayList<>();
-
-        current.add(valueIndexPairs[0]);
-        for(int i = 1; i < n; i++)
+        for (int i = 0; i < srt.length; i++) 
         {
-            if(valueIndexPairs[i][0] - valueIndexPairs[i - 1][0] <= limit)
+            if (i == 0 || srt[i] - srt[i - 1] > limit) 
             {
-                current.add(valueIndexPairs[i]);
+                grps.add(new ArrayList<>());
+                id++;
             }
-            else
-            {
-                groups.add(current);
-                current = new ArrayList<>();
-                current.add(valueIndexPairs[i]);
-            }
+            
+            grps.get(id).add(srt[i]);
+            map.put(srt[i], id);
         }
 
-        groups.add(current);
-        int[] result = new int[n];
-        for(ArrayList<int[]> group : groups)
+        int[] idx = new int[grps.size()];
+
+        for (int i = 0; i < A.length; i++) 
         {
-            ArrayList<Integer> values = new ArrayList<>();
-            ArrayList<Integer> index = new ArrayList<>();
-
-            for(int[] pair : group)
-            {
-                values.add(pair[0]);
-                index.add(pair[1]);
-            }
-
-            Collections.sort(index);
-            for(int i = 0; i < index.size(); i++)
-            {
-                result[index.get(i)] = values.get(i);
-            }
+            int cur = map.get(A[i]);
+            A[i] = grps.get(cur).get(idx[cur]);
+            idx[cur]++;
         }
 
-        return result;
+        return A;
     }
 }
